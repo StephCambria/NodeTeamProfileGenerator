cb36f29648bbece7808f35122c06df8192d1b3de
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-09-10 17:10:24 -0400

    minor tweaks

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const Manager = require('./lib/manager');
 const Intern = require('./lib/intern');
 const Engineer = require('./lib/engineer');
 // importing site generation files
-// const generateSite = require('./src/generate-site.js');
+const generateSite = require('./src/generate-site.js');
 const fs = require("fs");
 const path = require("path");
 // directory files
- const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 // array to hold team members
 const teamMembers = [];
 
