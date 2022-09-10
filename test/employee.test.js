d502f00a4de683da4e04b42bbf8f17c2fc69a97e
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-09-10 15:52:20 -0400

    starting the file structure

---
+++ test/employee.test.js
@@ -0,0 +1,35 @@
+// use jest to run testing
+// create lib folder in directory later!
+
+// =====================================================
+
+// const Employee = require('../lib/employee);
+// const employee = new Employee('kristynna', '100297', 'kmvaldes@fakemail.com);
+
+// =====================================================
+
+// test('test if we can get the constructor values for the employee object', () => {
+//      expect(employee.name).toBe('kristynna');
+//      expect(employee.id).toBe('100297');
+//      expect(employee.email).toBe('kmvaldes@fakemail.com');
+// });
+
+
+// test('test if we can get the name from the getName() method', () => {
+//      expect(employee.getName()).toBe('kristynna');
+// });
+
+
+// test('test if we can get the id from the getId() method', () => {
+//      expect(employee.getId()).toBe('100297');
+// });
+
+
+// test('test if we can get the email from the getEmail() method', () => {
+//      expect(employee.getEmail()).toBe('kmvaldes@fakemail.com');
+// });
+
+
+// test('test if we can get the role from the getRole() method', () => {
+//      expect(employee.getRole()).toBe('Employee');
+// });
---
+++ test/manager.test.js
@@ -0,0 +1,35 @@
+// const Manager = require('../lib/manager');
+// const manager = new Manager('donnie', '032293', 'dmurray@fakemail.com', '290');
+
+// repeat the testing cycle from employee.test.js!
+
+// test('test if we can get the constructor values for the employee object', () => {
+//      expect(manager.name).toBe('donnie');
+//      expect(manager.id).toBe('032293');
+//      expect(manager.email).toBe('dmurray@fakemail.com');
+//      expect(manager.officeNumber).toBe('290');
+// });
+
+
+// test('test if we can get the name from the getName() method', () => {
+//      expect(manager.getName()).toBe('donnie');
+// });
+
+
+// test('test if we can get the id from the getId() method', () => {
+//      expect(manager.getId()).toBe('032294');
+// });
+
+
+// test('test if we can get the email from the getEmail() method', () => {
+//      expect(manager.getEmail()).toBe('dmurray@fakemail.com');
+// });
+
+// test('test if we can get the office number from the getOfficeNumber() method', () => {
+//      expect(manager.getOfficeNumber()).toBe('290');
+// });
+
+
+// test('test if we can get the role from the getRole() method', () => {
+//      expect(manager.getRole()).toBe('Manager');
+// });
