d71518f9a7a0def23bd2b7ee9162a879e558472e
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-09-10 16:04:20 -0400

    adding more tests

---
+++ test/intern.test.js
@@ -0,0 +1,35 @@
+// const Intern = require('../lib/intern');
+// const intern = new Intern('Elliot', '091394', 'ereynolds@fakemail.com', 'Blaina');
+
+// repeat the testing cycle from employee.test.js!
+
+// test('test if we can get the constructor values for the intern object', () => {
+//      expect(intern.name).toBe('elliot');
+//      expect(intern.id).toBe('091394');
+//      expect(intern.email).toBe('ereynolds@fakemail.com');
+//      expect(intern.school).toBe('Blaina');
+// });
+
+
+// test('test if we can get the name from the getName() method', () => {
+//      expect(intern.getName()).toBe('elliot');
+// });
+
+
+// test('test if we can get the id from the getId() method', () => {
+//      expect(intern.getId()).toBe('091394');
+// });
+
+
+// test('test if we can get the email from the getEmail() method', () => {
+//      expect(intern.getEmail()).toBe('ereynolds@fakemail.com');
+// });
+
+// test('test if we can get the school location from the getSchool() method', () => {
+//      expect(intern.getSchool()).toBe('Blaina');
+// });
+
+
+// test('test if we can get the role from the getRole() method', () => {
+//      expect(intern.getRole()).toBe('Intern');
+// });
