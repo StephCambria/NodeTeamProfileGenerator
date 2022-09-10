6274de6f3886f68fc9dc59d2c956b933896ff3c9
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-09-10 17:31:54 -0400

    organizing

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -1,35 +1,36 @@
-// const Engineer = require('../lib/engineer');
-// const engineer = new Engineer('stephanie', '082994', 'scambria@fakemail.com', 'StephCambria');
+const Engineer = require("../lib/engineer");
+const engineer = new Engineer(
+  "stephanie",
+  "082994",
+  "scambria@fakemail.com",
+  "StephCambria"
+);
 
 // repeat the testing cycle from employee.test.js!
 
-// test('test if we can get the constructor values for the engineer object', () => {
-//      expect(engineer.name).toBe('stephanie');
-//      expect(engineer.id).toBe('082994');
-//      expect(engineer.email).toBe('scambria@fakemail.com');
-//      expect(engineer.githubUsername).toBe('StephCambria');
-// });
+test("test if we can get the constructor values for the engineer object", () => {
+  expect(engineer.name).toBe("stephanie");
+  expect(engineer.id).toBe("082994");
+  expect(engineer.email).toBe("scambria@fakemail.com");
+  expect(engineer.githubUsername).toBe("StephCambria");
+});
 
+test("test if we can get the name from the getName() method", () => {
+  expect(engineer.getName()).toBe("stephanie");
+});
 
-// test('test if we can get the name from the getName() method', () => {
-//      expect(engineer.getName()).toBe('stephanie');
-// });
+test("test if we can get the id from the getId() method", () => {
+  expect(engineer.getId()).toBe("082994");
+});
 
+test("test if we can get the email from the getEmail() method", () => {
+  expect(engineer.getEmail()).toBe("scambria@fakemail.com");
+});
 
-// test('test if we can get the id from the getId() method', () => {
-//      expect(engineer.getId()).toBe('082994');
-// });
+test("test if we can get the github username from the getGithubUsername() method", () => {
+  expect(engineer.getGithubUsername()).toBe("StephCambria");
+});
 
-
-// test('test if we can get the email from the getEmail() method', () => {
-//      expect(engineer.getEmail()).toBe('scambria@fakemail.com');
-// });
-
-// test('test if we can get the github username from the getGithubUsername() method', () => {
-//      expect(engineer.getGithubUsername()).toBe('StephCambria');
-// });
-
-
-// test('test if we can get the role from the getRole() method', () => {
-//      expect(engineer.getRole()).toBe('Engineer');
-// });
+test("test if we can get the role from the getRole() method", () => {
+  expect(engineer.getRole()).toBe("Engineer");
+});
