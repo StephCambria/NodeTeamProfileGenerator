1d7278e07e6d84d1a0a7bb6a939494468270d682
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-09-10 17:34:35 -0400

    organizing

--- test/manager.test.js
+++ test/manager.test.js
@@ -1,35 +1,36 @@
-// const Manager = require('../lib/manager');
-// const manager = new Manager('stephanie', '082994', 'scambria@fakemail.com', '290');
+const Manager = require("../lib/manager");
+const manager = new Manager(
+  "stephanie",
+  "082994",
+  "scambria@fakemail.com",
+  "290"
+);
 
 // repeat the testing cycle from employee.test.js!
 
-// test('test if we can get the constructor values for the employee object', () => {
-//      expect(manager.name).toBe('stephanie');
-//      expect(manager.id).toBe('082994');
-//      expect(manager.email).toBe('scambria@fakemail.com');
-//      expect(manager.officeNumber).toBe('280');
-// });
+test("test if we can get the constructor values for the employee object", () => {
+  expect(manager.name).toBe("stephanie");
+  expect(manager.id).toBe("082994");
+  expect(manager.email).toBe("scambria@fakemail.com");
+  expect(manager.officeNumber).toBe("280");
+});
 
+test("test if we can get the name from the getName() method", () => {
+  expect(manager.getName()).toBe("stephanie");
+});
 
-// test('test if we can get the name from the getName() method', () => {
-//      expect(manager.getName()).toBe('stephanie');
-// });
+test("test if we can get the id from the getId() method", () => {
+  expect(manager.getId()).toBe("082994");
+});
 
+test("test if we can get the email from the getEmail() method", () => {
+  expect(manager.getEmail()).toBe("scambria@fakemail.com");
+});
 
-// test('test if we can get the id from the getId() method', () => {
-//      expect(manager.getId()).toBe('082994');
-// });
+test("test if we can get the office number from the getOfficeNumber() method", () => {
+  expect(manager.getOfficeNumber()).toBe("280");
+});
 
-
-// test('test if we can get the email from the getEmail() method', () => {
-//      expect(manager.getEmail()).toBe('scambria@fakemail.com');
-// });
-
-// test('test if we can get the office number from the getOfficeNumber() method', () => {
-//      expect(manager.getOfficeNumber()).toBe('280');
-// });
-
-
-// test('test if we can get the role from the getRole() method', () => {
-//      expect(manager.getRole()).toBe('Manager');
-// });
+test("test if we can get the role from the getRole() method", () => {
+  expect(manager.getRole()).toBe("Manager");
+});
