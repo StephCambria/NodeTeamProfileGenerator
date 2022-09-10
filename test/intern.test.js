8562719377f27eedb39ad8457f7ef40a1672c589
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-09-10 17:33:18 -0400

    organizing

--- test/intern.test.js
+++ test/intern.test.js
@@ -1,35 +1,36 @@
-// const Intern = require('../lib/intern');
-// const intern = new Intern('stephanie', '082994', 'scambria@fakemail.com', 'boston');
+const Intern = require("../lib/intern");
+const intern = new Intern(
+  "stephanie",
+  "082994",
+  "scambria@fakemail.com",
+  "boston"
+);
 
-// repeat the testing cycle from employee.test.js!
+//repeat the testing cycle from employee.test.js!
 
-// test('test if we can get the constructor values for the intern object', () => {
-//      expect(intern.name).toBe('stephanie');
-//      expect(intern.id).toBe('082994');
-//      expect(intern.email).toBe('scambria@fakemail.com');
-//      expect(intern.school).toBe('boston');
-// });
+test("test if we can get the constructor values for the intern object", () => {
+  expect(intern.name).toBe("stephanie");
+  expect(intern.id).toBe("082994");
+  expect(intern.email).toBe("scambria@fakemail.com");
+  expect(intern.school).toBe("boston");
+});
 
+test("test if we can get the name from the getName() method", () => {
+  expect(intern.getName()).toBe("stephanie");
+});
 
-// test('test if we can get the name from the getName() method', () => {
-//      expect(intern.getName()).toBe('stephanie');
-// });
+test("test if we can get the id from the getId() method", () => {
+  expect(intern.getId()).toBe("082994");
+});
 
+test("test if we can get the email from the getEmail() method", () => {
+  expect(intern.getEmail()).toBe("scambria@fakemail.com");
+});
 
-// test('test if we can get the id from the getId() method', () => {
-//      expect(intern.getId()).toBe('082994');
-// });
+test("test if we can get the school location from the getSchool() method", () => {
+  expect(intern.getSchool()).toBe("boston");
+});
 
-
-// test('test if we can get the email from the getEmail() method', () => {
-//      expect(intern.getEmail()).toBe('scambria@fakemail.com');
-// });
-
-// test('test if we can get the school location from the getSchool() method', () => {
-//      expect(intern.getSchool()).toBe('boston');
-// });
-
-
-// test('test if we can get the role from the getRole() method', () => {
-//      expect(intern.getRole()).toBe('Intern');
-// });
+test("test if we can get the role from the getRole() method", () => {
+  expect(intern.getRole()).toBe("Intern");
+});
